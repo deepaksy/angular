cda0e43752f923722f1f862376aafaf30fc0c75f
deepaksy <deepaksy28@gmail.com>
2022-03-14 13:05:02 +0530

    added getDate Method in date.service.ts

--- src/app/date.service.ts
+++ src/app/date.service.ts
@@ -9,5 +9,9 @@ export class DateService {
     console.log(this.getdate)
    }
   getdate:Date = new Date();
+
+  getDate(day:number,month:number,year:number):Date{
+    return new Date(year,month,day);
+  }
   
 }

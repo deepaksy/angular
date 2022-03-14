9d654310c8959d504098ed0ee2b534bac2076124
deepaksy <deepaksy28@gmail.com>
2022-03-14 09:14:13 +0530

    Add MatCheckBoxModule in maetial.module.ts

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -12,6 +12,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 const MaterialComponents =[
   MatButtonModule,
   MatGridListModule,
@@ -27,6 +28,7 @@ const MaterialComponents =[
   MatListModule,
   MatDividerModule,
   MatCardModule,
+  MatCheckboxModule
 ]
 @NgModule({
   imports: [MaterialComponents],
